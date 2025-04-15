4b7b033a0b3e8755cda304b397d30a4c4b18ef8f
Stephen Soltan <187546598+saltman007web@users.noreply.github.com>
2025-04-15 14:34:44 +0930

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,3 @@
-// swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -10,7 +7,6 @@ let package = Package(
         .tvOS(.v15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AppdbFramework",
             targets: [
@@ -19,8 +15,6 @@ let package = Package(
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
        .binaryTarget(
            name: "AppdbRemoteBinaryPackage",
            url: "https://dbservices.to/lib-dist/AppdbFramework-1.6.2.xcframework.zip",
